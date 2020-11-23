75e68bfcfef5fd62c565f2f57a4b9cc85ed09fda
nanmar gudino <desarrollo2.acero@gmail.com>
2020-11-23 00:35:13 -0400

    it has solar style for the entire app, partial landing, and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module'
+import { AuthModule } from './auth/auth.module';
 
 
 
@@ -17,6 +18,9 @@ import { AuthModule } from './auth/auth.module'
     AppRoutingModule,
     SharedModule,
     AuthModule,
+    BrowserAnimationsModule,
+    SharedModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,11 +4,16 @@ import { AuthRoutingModule } from './aut-routing.module'
 import { LoginComponent} from '../auth/login/login.component';
 import { RegisterComponent } from '../auth/register/register.component';
 
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
 @NgModule({
  declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ButtonModule,
+    CardModule,
   ],
  
 })
