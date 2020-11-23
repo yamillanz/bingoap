bed67986dd6c6ff6e16c989d9b96af680804c2fc
nanmar gudino <desarrollo2.acero@gmail.com>
2020-11-22 20:10:40 -0400

    delenting hash # for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
 @NgModule({
 	imports: [
 		RouterModule.forRoot(routes,{
-			useHash: true,
+			//useHash: true,
 			anchorScrolling: 'enabled'
 		}),
 		NgxsModule.forRoot([], {
