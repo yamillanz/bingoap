02374fb0eba8f5bc79c82c2a169b25baaafc8862
Yamil Lanz <ylanz@2050net.local>
2020-12-02 07:36:07 -0400

    format

--- src/app/auth/interceptor.service.ts
+++ src/app/auth/interceptor.service.ts
@@ -4,32 +4,32 @@ import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest
 import { Observable, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor {
 
-  constructor() { }
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // throw new Error('Method not implemented.');
-    console.log('por el interceptor');
+	constructor() { }
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		// throw new Error('Method not implemented.');
+		console.log('por el interceptor');
 
-    const reqClone = req.clone({
-      // Headers
-    });
-    return next.handle(reqClone).pipe(
-      catchError(this.handleError)
+		const reqClone = req.clone({
+			// Headers
+		});
+		return next.handle(reqClone).pipe(
+			catchError(this.handleError)
 
-    );
+		);
 
-  }
+	}
 
-  handleError(err: HttpErrorResponse) {
-    let errorMessage = 'un error en el retorno de la data';
-    if (err) {
-      errorMessage = `Error: code ${err.message}`;
-    }
+	handleError(err: HttpErrorResponse) {
+		let errorMessage = 'un error en el retorno de la data';
+		if (err) {
+			errorMessage = `Error: code ${err.message}`;
+		}
 
-    return throwError(errorMessage);
-  }
+		return throwError(errorMessage);
+	}
 
 }
