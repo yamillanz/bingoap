0bd892a3c4e473e1dc5059d951ce85a58b29fc00
Yamil Lanz <ylanz@2050net.local>
2020-12-02 09:55:00 -0400

    Testeado un BSubject global, no sirvio

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -11,7 +11,6 @@ import { environment } from '../../../environments/environment';
 })
 export class AuthService {
 
-	private userBh : User = {};
 
 	private userData$: BehaviorSubject<User> = new BehaviorSubject<User>(this._initBehavior());
 
