03a11f64b66a6c6c35b772a42dd945719d4ba626
nanmar gudino <desarrollo2.acero@gmail.com>
2020-12-02 17:18:47 -0400

    commiting before pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-/* import { HttpClientModule } from '@angular/common/http'; */
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 
-/* import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+/* import { HttpClientModule } from '@angular/common/http'; */
 
+
+/* import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 const config: SocketIoConfig = { url: 'http://localhost:4001', options: {} }; */
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule,
-    AuthModule,
-    SharedModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		AppRoutingModule,
+		SharedModule,
+		// SocketIoModule.forRoot(config)
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
